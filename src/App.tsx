ea8be46bc67f65eae8d1c1a86b587e910d364649
Intiser Rajoan Parash <irparash@gmail.com>
2023-01-20 16:30:14 -0600

    Add ESLint and Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
-import reactLogo from './assets/react.svg'
-import changePlusPlusLogo from './assets/changeplusplus.png'
-import './App.css'
+import "./App.css";
+import changePlusPlusLogo from "./assets/changeplusplus.png";
+import reactLogo from "./assets/react.svg";
 
 function App() {
   return (
     <div className="app">
       <div>
-        <a href="https://www.changeplusplus.org" target="_blank">
+        <a
+          href="https://www.changeplusplus.org"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src={changePlusPlusLogo} className="logo" alt="Change++ logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>ChangePlusPlus + React = ❤️</h1>
       <p>React TypeScript ESLint Prettier Template</p>
-        <p>Start editing from <code>src/App.tsx</code></p>
+      <p>
+        Start editing from <code>src/App.tsx</code>
+      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
